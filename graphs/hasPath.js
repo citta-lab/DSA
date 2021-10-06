d407fa37aa38bc63821af8c7ca169d520ffa38f6
mahesh <mahesh2684@gmail.com>
2021-10-05 23:50:19 -0400

    recursion

--- graphs/hasPath.js
+++ graphs/hasPath.js
@@ -32,8 +32,33 @@ const hasPath = (graph, rootNode, destNode) => {
     return false;
 }
 
+console.log('************* iterative dfs *****************')
 console.log(hasPath(graph, 'a', 'f')); // true
 console.log(hasPath(graph, 'a', 'e')); // true
 console.log(hasPath(graph, 'a', 'a')); // true
 console.log(hasPath(graph, 'c', 'g')); // true
 console.log(hasPath(graph, 'c', 'a')); // false
+
+
+
+/** Recursion for DFS */
+const hasPathRecursion = (graph, rootNode, destNode) => {
+    
+    if(rootNode === destNode) return true;
+
+    const children = graph[rootNode];
+    for(let child of children){
+        if(child === destNode) return true;
+        let result = hasPathRecursion(graph, child, destNode);
+        if(result) return true;
+    }
+
+    return false;
+}
+
+console.log('************* recursion dfs *****************')
+console.log(hasPathRecursion(graph, 'a', 'f')); // true
+console.log(hasPathRecursion(graph, 'a', 'e')); // true
+console.log(hasPathRecursion(graph, 'a', 'a')); // true
+console.log(hasPathRecursion(graph, 'c', 'g')); // true
+console.log(hasPathRecursion(graph, 'c', 'a')); // false

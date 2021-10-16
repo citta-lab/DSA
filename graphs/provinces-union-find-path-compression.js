992165f211dd5cd9ec0d990425068d5b21b900df
mahesh <mahesh2684@gmail.com>
2021-10-16 18:58:20 -0400

    findPArent

---
+++ graphs/provinces-union-find-path-compression.js
@@ -0,0 +1,86 @@
+/** Given an two dimentional array of water ( 0 ) and land ( 1 ) calculate number of
+ * island.
+ * 
+ * Important:
+ * This is just a different variation of unidrectional component count graph problem.
+ * However the given data is represnted in two dimentional array and is still a unidirectional.
+ * 
+ * Use:
+ * We will use DFS as we can go as deep as we can with in a island and also problem is
+ * not about SHORTEST path. 
+ * 
+ * LEET CODE QUESTIONS:
+ * https://leetcode.com/problems/number-of-islands/submissions/
+ * https://leetcode.com/problems/number-of-provinces/solution/ fails when input is [[1,0,0,1],[0,1,1,0],[0,1,1,1],[1,0,1,1]] if we use dfs recursion. So union find is the solution. 
+ */
+
+ /** given m*n grid of water/land */
+ const grid = [
+     [1,0,0,1],
+     [0,1,1,0],
+     [0,1,1,1],
+     [1,0,1,1]
+    ];
+
+/**
+ * @param {number[][]} isConnected
+ * @return {number}
+ */
+var findCircleNum = function(isConnected) {
+    const parent = buildParent(isConnected.length);
+    let components = isConnected.length;
+    
+    for(let i=0; i < isConnected.length; i++){
+        for(let j=0; j < isConnected.length; j++){
+            if(isConnected[i][j] === 1){
+                let p1 = findParent(i, parent);
+                let p2 = findParent(j, parent);
+                
+                if(p1 != p2){
+                    parent[p1] = p2;
+                    components -= 1
+                }
+            }
+        }
+    }
+    
+    return components;
+};
+
+const buildParent = (n, array=[]) => {
+    for(let i=0; i<n; i++){
+        array.push(i)
+    }
+    
+    return array;
+}
+
+const findParent = (node, parent) =>{  
+    if(node != parent[node]){
+        let rootNode = findParent(parent[node], parent);
+        parent[node] = rootNode;
+    }
+    return parent[node];
+}
+
+/** flavor one: alternative findParent options */
+const findParentTwo = (node, parent) => {
+    if(node === parent[node]) return node;
+
+    let rootNode =  findParent(parent[node], parent);
+    parent[node] = rootNode;
+    return rootNode;
+}
+
+/** flavor two: alternative findParent options */
+const findParentThree = (node, parent) => {
+    if (parent[node] != node) {
+        parent[node] = findParent(parent[node], parent);
+    }
+    return parent[node];
+}
+
+console.log(findCircleNum(grid)); // 1 ( if we get 4 then wrong )
+console.log(findCircleNum([[1,1,0],[1,1,0],[0,0,1]])); // 2
+console.log(findCircleNum([[1,0,0],[0,1,0],[0,0,1]])); // 3
+console.log(findCircleNum([[1,0,0,1],[0,1,1,0],[0,1,1,1],[1,0,1,1]])); // 1 

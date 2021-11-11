2ebfbb4b7fa7121939f15c5507150a5542d787f3
mahesh <mahesh2684@gmail.com>
2021-11-10 19:32:11 -0500

    binary search for array : Nov 10th 2021

---
+++ arrays/binary-search-array.js
@@ -0,0 +1,67 @@
+
+/** 
+ * Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. 
+ * If target exists, then return its index. Otherwise, return -1.
+ * You must write an algorithm with O(log n) runtime complexity. 
+ * 
+ * leetcode : https://leetcode.com/problems/binary-search/
+ * 
+ * */
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var search = function(nums, target) {
+    return binarySearch(nums, target, 0, nums.length-1);
+ };
+ 
+ const binarySearch = (nums, target, left, right) => {
+     
+     if(left > right) return -1;
+     
+     let mid = Math.floor((left+right) / 2); 
+     
+     if(nums[mid] === target) return mid; 
+     
+     if(target < nums[mid]) return binarySearch(nums, target, left, mid-1);
+     if(target > nums[mid]) return binarySearch(nums, target, mid+1, right);
+ };
+
+
+ const binarySearchIterative = (nums, target) => {
+    
+    let left = 0; 
+    let right = nums.length - 1;
+    /** if we dont use <= it will fail for array with one element */
+    while(left <= right){
+        
+        //let mid = Math.floor(left+ (right - left / 2)); <-- if larger number
+        let mid = Math.floor((left+right) / 2);
+        
+        if(nums[mid] === target) return mid;
+        
+        if(target < nums[mid]) {
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+    
+    return -1;   
+}
+
+
+
+let arr = [-1,0,3,5,9,12];
+console.log(search(arr, 9)); // 4
+console.log(binarySearchIterative ( arr, 9));  // 4
+
+arr = [-1,0,3,5,9,12];
+console.log(search(arr, 2)); // -1
+console.log(binarySearchIterative(arr, 2));  // -1
+
+arr = [2];
+console.log(search(arr, 2)); // 0
+console.log(binarySearchIterative(arr, 2)); // 0

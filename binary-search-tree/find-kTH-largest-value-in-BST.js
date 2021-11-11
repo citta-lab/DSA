ff945e91c7b4182378e956f34df4597fe4292b58
mahesh <mahesh2684@gmail.com>
2021-11-11 00:10:03 -0500

    binary search for array : Nov 10th 2021

---
+++ binary-search-tree/find-kTH-largest-value-in-BST.js
@@ -0,0 +1,58 @@
+/** 
+ * Given a valid binary search tree and a kth value which represents the K'th largest value in BST.
+ * Example: if the BST has values ranging from 1 to 9 and k = 2 then k'th largest value in BST is
+ * 8 as 9 is the first largest and 8 is the second largest.
+ * 
+ * Hint: Remember it is BST. So values are sorted and all we need to do is apply the correct traversal.
+ */
+
+const { buildTree } = require('./BinaryTree');
+
+const findKthLargestValue = (tree, k ) => {
+
+    /** 
+     * hence it's a BST, all values on left of nodes are less than
+     * root and all values on right are greater. So if we apply 
+     * INORDER TRAVERSAL ( left, root, right) we will have sorted array
+     * in ascending order. Then we can simply return value at size-k piosition */
+
+    const result = inOrderTraversal(tree); // returns array;
+    const size = result.length;
+    const kThPositionFromTail = size - k;
+
+    return result[kThPositionFromTail];
+}
+
+const inOrderTraversal = (tree, array=[]) => {
+    
+    /** left, root, right */
+    
+    let stack = [];
+    let cur = tree;
+
+    while(cur || stack.length){
+        while(cur){
+            stack.push(cur);
+            cur = cur.left;
+        }
+
+        /** now we have all left nodes in, so process root and then go to right */
+        let node = stack.pop();
+        array.push(node.val);
+
+        cur = node.right; // this will rerun the while loop with right node as cur
+    }
+
+    return array;
+}
+
+let tree  = buildTree([5,2,6,1,3]);
+console.log(findKthLargestValue(tree, 2));  // 5
+
+
+
+tree = buildTree([15,5,20,2,5,17,22,1,3]);
+console.log(findKthLargestValue(tree, 2)) // 20
+
+tree = buildTree([15,5,20,2,5,17,22,1,3]);
+console.log(findKthLargestValue(tree, 3)) // 17
--- trees/binary-tree-level-order.js
+++ trees/binary-tree-level-order.js
@@ -45,12 +45,52 @@ var levelOrder = function(root) {
 };
 
 
+/** with right push and shift methods  */
+var levelOrderII = function(root) {
+    
+    
+    let queue = [];
+    let current = root;
+    
+    /** prevents adding empty i.e [ ] to the queue */
+    if(current) queue.push(current);
+    
+    let result = [];
+    while(queue.length){
+       
+        let temp = [];
+        let size = queue.length; 
+        
+        /** 
+           insertion to the queue happens similar to recursion stack, so if we depend on
+           queue size we are literally tracking levels of the tree. Example: First time 
+           queue has size one due to root (for loop runs once ), so root element is added
+           to temp, then to result. second time queue has root's left and right so size is
+           two for loops ends after dequeing two nodes and continues */
+        for(let i=0; i < size; i++){
+            
+            let node = queue.shift();
+            if(node) temp.push(node.val);
+
+            if(node && node.left) queue.push(node.left);
+            if(node && node.right) queue.push(node.right);
+        }
+        
+        result.push(temp);
+    }
+    
+    return result;
+};
+
+
+
+
 let tree = buildTree([3,9,20,15,7]);
-console.log(levelOrder(tree)); // [ [ 3 ], [ 9, 20 ], [ 15, 7 ] ]
+console.log(levelOrderII(tree)); // [ [ 3 ], [ 9, 20 ], [ 15, 7 ] ]
 
 tree = buildTree([3,9]);
-console.log(levelOrder(tree)); // [ [ 3 ], [ 9 ] ]
+console.log(levelOrderII(tree)); // [ [ 3 ], [ 9 ] ]
 
 tree = buildTree([]);
-console.log(levelOrder(tree)); // []
+console.log(levelOrderII(tree)); // []
 

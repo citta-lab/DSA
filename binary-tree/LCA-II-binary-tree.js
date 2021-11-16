00c5e58157231476473a817ab6322259691c4c13
mahesh <mahesh2684@gmail.com>
2021-11-16 00:17:27 -0500

    LCA binary tree: Nov 16th 2021

--- binary-tree/LCA-II-binary-tree.js
+++ binary-tree/LCA-II-binary-tree.js
@@ -10,6 +10,43 @@
  * In https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/ both nodes are for sure present
  * */
 
+
+var lowestCommonAncestor = function(root, p, q) {
+    return LCA(root, p, q);
+};
+
+function LCA( root, p, q){
+   
+    let count = 0;
+    
+    function helper(root, p, q){
+        if(!root) return root;
+    
+        let left = helper(root.left, p, q);
+        let right = helper(root.right, p, q);
+        
+        /** need to happen after left and right check */
+        if(root === p || root === q){
+            count ++
+            return root;
+        }
+
+        if( left && right) return root;
+        
+        return left ? left : right;
+    }
+    
+    let lca = helper(root, p, q);
+    return count === 2 ? lca : null;
+}
+
+
+
+
+/** BELOW ARE EXAMPLES WHERE I MGITH HAVE DONE WRONG */
+
+
+
 /** FAILURE : this WORKS if both p and q for sure exist  */
 const LCA = (root, p, q) => {
     if(!root) return null; 
@@ -24,7 +61,7 @@ const LCA = (root, p, q) => {
     return left ? left : right; // <-- as it doesnt check if p or q doesnt even present
 }
 
-/** GOOD:  but global count get overridden so leet code second examples onward will fail */
+/** GOOD:  but global `count` get overridden so leet code second examples onward will fail */
 var lowestCommonAncestor = function(root, p, q) {
     let LCA = LCAHelper ( root, p, q);
     // if we get 2 count then it means we found both p and q
@@ -44,6 +81,7 @@ const LCAHelper = ( root, p, q) => {
     let left = LCAHelper(root.left, p, q);
     let right = LCAHelper(root.right, p, q);
     
-    /** important to do this order of check left null then right, right null then root */
-    return left === null ? right : right === null ? left : root;
+    if( left && right) return root;
+        
+    return left ? left : right;
 }

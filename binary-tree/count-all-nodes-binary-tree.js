104335246f0d216569752a83837548d3ce892729
mahesh <mahesh2684@gmail.com>
2021-12-11 00:20:03 -0500

    count

--- binary-tree/count-all-nodes-binary-tree.js
+++ binary-tree/count-all-nodes-binary-tree.js
@@ -6,6 +6,7 @@ Input: root = [1,2,3,4,5,6]
 Output: 6
 
 leetcode : https://leetcode.com/problems/count-complete-tree-nodes/
+video: https://www.youtube.com/watch?v=4wPlA_InnGY
 
 IMPORTANT: 
 question said COMPLETE tree. So depth of the tree on left and right is same but might not be balanced
@@ -13,13 +14,36 @@ SOLVE with O(1) space and O(log n) time
 
 */
 
+/**
+ * Time: O(log N * log N) where N is node. In this we go left and right to calculate the height of balance
+ * tree which costs us log 2 and in worst case we will do both left and right. Hence log n * log n
+ * Space: O(1) */
+var countNodes = function(root) {
+   
+    if(!root) return 0;
+    
+    let leftHeight = (root) => {
+        if(!root) return 0;
+        return 1 + leftHeight(root.left);
+    }
+    
+    let rightHeight = (root) => {
+        if(!root) return 0;
+        return 1 + rightHeight(root.right);
+    }
+    
+    if(leftHeight === rightHeight){
+        return Math.pow(2, leftHeight) - 1;
+    }
+    
+   
+    return 1 + countNodes(root.left) + countNodes(root.right);
+};
+
 /** BrutForce : O(N) time and O(log N) space */
 var countNodes = function(root) {
     return root ? 1 + countNodes(root.left) + countNodes(root.right) : 0
 };
-
-
-
 
 
 /** EASY preOrder appraoch */

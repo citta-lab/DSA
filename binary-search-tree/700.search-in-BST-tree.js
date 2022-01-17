a3c643a9e8cc162329a32c4f7256dd55a9e5b3a9
citta-lab <mahesh2684@gmail.com>
2022-01-17 09:17:57 -0500

    Update and rename search-in-BST-tree.js to 700.search-in-BST-tree.js

--- binary-search-tree/700.search-in-BST-tree.js
+++ binary-search-tree/700.search-in-BST-tree.js
@@ -1,5 +1,7 @@
 
 /** 
+700. Search in a Binary Search Tree
+
 You are given the root of a binary search tree (BST) and an integer val. 
 Find the node in the BST that the node's value equals val and return the subtree rooted with that node.
 If such a node does not exist, return null. 
@@ -8,6 +10,7 @@ Input: root = [4,2,7,1,3], val = 2
 Output: [2,1,3]
 
 leetcode: https://leetcode.com/problems/search-in-a-binary-search-tree/
+leetcode-question:700
 
 */
 

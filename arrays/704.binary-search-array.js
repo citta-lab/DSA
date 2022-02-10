ff75bb907df56a4cee9215b2f54171def323c968
citta-lab <mahesh2684@gmail.com>
2022-02-10 17:43:49 -0500

    Update and rename binary-search-array.js to 704.binary-search-array.js

--- arrays/704.binary-search-array.js
+++ arrays/704.binary-search-array.js
@@ -1,9 +1,12 @@
 
 /** 
+ * 704. Binary Search
+ * 
  * Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. 
  * If target exists, then return its index. Otherwise, return -1.
  * You must write an algorithm with O(log n) runtime complexity. 
  * 
+ * leetcode-problem:704
  * leetcode : https://leetcode.com/problems/binary-search/
  * 
  * */

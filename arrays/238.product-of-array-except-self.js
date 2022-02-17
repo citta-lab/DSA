1f169ea2667e0ca9e57e3f8e7fabb9f6dc30e832
citta-lab <mahesh2684@gmail.com>
2022-02-17 08:40:48 -0500

    Create 238.product-of-array-except-self.js

---
+++ arrays/238.product-of-array-except-self.js
@@ -0,0 +1,69 @@
+/** 
+
+238. Product of Array Except Self
+
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+You must write an algorithm that runs in O(n) time and without using the division operation.
+
+Input: nums = [1,2,3,4]
+Output: [24,12,8,6]
+
+Input: nums = [-1,1,0,-3,3]
+Output: [0,0,9,0,0]
+
+leetcode-question:238
+leetcode:https://leetcode.com/problems/product-of-array-except-self/
+
+
+Hint:
+- BruteForce: O(n^2)
+- Traverse an array in for loop. Then have left and right pointer.
+- Have while loop for left pointer which goes left of the current num and find product 
+- Have while loop for right pointer which goes right of the current num and finds product
+- Multiply the left and right produt 
+
+- Optimal: Time: O(n) and Space: O(n)
+- Similar to above but we prepare left and right product array seperate 
+- start of the left product array will be 1 i.e leftProductArray[0] = 1
+- end of the right product array will be 1 i.e rightProductArray[nums.length-1] = 1
+- then we calculate leftProduct array goes --> and rightProductArray goes <--
+- final result will be product of these values
+
+*/
+
+
+
+/** Time: O(n^2) and Space: O(n) */
+var productExceptSelf = function(nums) {
+    
+    let result = new Array(nums.length).fill(0);
+    
+    let left =0;
+    let right =0; 
+    
+    for(let i=0; i<nums.length; i++){
+        let cur = nums[i];
+        
+        let left = i-1;
+        let right = i+1;
+        
+        let leftProduct = 1;
+        while(left >= 0){
+            leftProduct = leftProduct * nums[left];
+            left --
+        }
+        
+        let rightProduct = 1;
+        while(right < nums.length){
+            rightProduct = rightProduct * nums[right];
+            right++
+        }
+        
+        let curExceptSelfProduct = leftProduct * rightProduct;
+        result[i] = curExceptSelfProduct;
+    }
+    
+    return result;
+};
+

b34bdb11c6d42b145a68c2a014b5568a31cd91b0
mahesh <mahesh2684@gmail.com>
2022-02-19 00:24:47 -0500

    graph: feb 18th 2022

---
+++ binary-tree/113.path-sum-II.js
@@ -0,0 +1,67 @@
+/**
+ * 
+ * 113. Path Sum II
+ * 
+ * Given the root of a binary tree and an integer targetSum, 
+ * return all root-to-leaf paths where the sum of the node values in the path 
+ * equals targetSum. Each path should be returned as a list of the node values, 
+ * not node references. A root-to-leaf path is a path starting from the root 
+ * and ending at any leaf node. A leaf is a node with no children.
+ * 
+ * Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22 
+ * Output: [[5,4,11,2],[5,8,4,5]]
+ * 
+ * Input: root = [1,2,3], targetSum = 5
+ * Output: []
+ * 
+ * leetcode-question:113
+ * leetcode:https://leetcode.com/problems/path-sum-ii/
+ * 
+ * Hint:
+ * - Simialar to Path Sum problem. Instead of returning true/false we just call dfs
+ * - Have two array, one holds all nodes while traversing and second one only holds
+ * path nodes when sum meets targetSum.
+ * - we also need running sum. 
+ * - dfs(root, targetSum, sum, pathNodes, pathList);
+ * - we will need to pop() last element from pathNodes as backtrack to go right from
+ * previous rootnode.
+ */
+
+ var pathSum = function(root, targetSum) {
+    let pathList = [];
+    let pathNodes = [];
+    dfs(root, targetSum, 0, pathNodes, pathList);
+    return pathList
+};
+
+function dfs(root, targetSum, sum, pathNodes, pathList){
+    
+    if(!root) return ;
+    
+    /** we will keep adding nodes to this array so if we do find
+    target sum at the end, we can simplay add this to pathList */
+    
+    pathNodes.push(root.val);
+    
+    /** 
+      if we reached leaf node and sum equals to target, 
+      we copy all pathNodes to pathList.
+    */
+    if(root && !root.left && !root.right){
+        if(sum + root.val === targetSum){
+            pathList.push([...pathNodes]);
+        }
+    } 
+    
+    dfs(root.left, targetSum, sum + root.val, pathNodes, pathList);
+    dfs(root.right, targetSum, sum + root.val, pathNodes, pathList);
+    
+    /** 
+    this will remove nodes from bottom one by one and reaches all
+    the way uptp root, then let root go right and starts popping back
+    
+    - also helpful to remove previously added node when sum
+    doesnt match the target sum
+    */
+    pathNodes.pop();
+}

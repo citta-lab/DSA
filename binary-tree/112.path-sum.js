00f451052082fbc508eb8bc5200f6995b164ed1e
mahesh <mahesh2684@gmail.com>
2022-02-18 22:15:20 -0500

    graph: feb 18th 2022

--- binary-tree/112.path-sum.js
+++ binary-tree/112.path-sum.js
@@ -1,9 +1,18 @@
 /** 
+ * 
+ * 112. Path Sum
+ * 
  * Given the root of a binary tree and an integer targetSum, return true if the tree has a root-to-leaf 
  * path such that adding up all the values along the path equals targetSum. A leaf is a node with no children. 
  * 
  * leetcode : https://leetcode.com/problems/path-sum/
  * video: https://www.youtube.com/watch?v=Hg82DzMemMI 
+ * 
+ * Hint
+ * - we can do TWO ways with using DFS. or BFS
+ * - we can DECREMENT the target sum with root.val and check if we reached left and target sum is 0
+ * - we can INCREMENT the sum with root.val and check if sum === target when root's left and right 
+ * values are null. i.e if(sum + root.val === targetSum) return true.
  * */
 
  const { buildTree } = require('./helper/BinaryTree');
@@ -18,6 +27,29 @@
         return hasPathSum(root.left, diff) || hasPathSum(root.right, diff);
     }
  }
+
+ /****************** OR  */
+
+ /** adding sum instead of subtracting */
+var hasPathSum = function(root, targetSum) {
+    return dfs(root, targetSum, 0);
+};
+
+function dfs(root, targetSum ,sum){
+    
+    if(!root) return false;
+    
+    if(root && !root.left && !root.right){
+        if(sum + root.val === targetSum){
+            return true
+        }
+    }
+    
+    let leftSum = dfs(root.left, targetSum, sum + root.val);
+    let rightSum = dfs(root.right, targetSum, sum + root.val);
+    
+    return leftSum || rightSum
+}
 
 let tree = buildTree([5,4,8,11,null,13,4,7,2,null,null,null,1]);
 console.log(hasPathSum(tree, 22));  // true

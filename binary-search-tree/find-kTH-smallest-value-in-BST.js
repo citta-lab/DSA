254cfe960bfd8e91875161b67641aea93462449b
mahesh <mahesh2684@gmail.com>
2022-02-21 23:39:53 -0500

    blind: feb 21th 2022

--- binary-search-tree/find-kTH-smallest-value-in-BST.js
+++ binary-search-tree/find-kTH-smallest-value-in-BST.js
@@ -1,4 +1,7 @@
 /** 
+ * 
+ * 230. Kth Smallest Element in a BST
+ * 
  * Given the root of a binary search tree, and an integer k, 
  * return the kth smallest value (1-indexed) of all the values
  *  of the nodes in the tree. 
@@ -6,11 +9,19 @@
  * Input: root = [3,1,4,null,2], k = 1
    Output: 1
  * 
+*  leetcode-question:230
  * leetcode: https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+ * BLIND:52(52/75)
  * */
 
 const { buildTree } = require('./helper/BinaryTree');
 
+/** 
+ * Time: O(N) i.e O(H+k) where H is height Space: O(N) in worst case and O(logN)
+ * in average balanced tree.
+ * 
+ * Recursion is second example
+ */
 var kthSmallest = function(root, k) {
     let cur = root;
     let stack = [];
@@ -31,3 +42,31 @@ var kthSmallest = function(root, k) {
     let indexPositionForKth = k - 1;
     return result[indexPositionForKth]
 };
+
+
+/** Recursion: Time: O(N) and space: O(N)  */
+var kthSmallest = function(root, k) {
+    
+    /** 
+    BST is already sorted and by doing inorder we can have
+    sorted array and simply return the k'th element from array */
+    
+    let arr = [];
+    BST(root, arr);
+    return arr[k-1]
+    
+};
+
+function BST(root, arr){
+    if(!root) return null
+    
+    if(root && root.left){
+        BST(root.left, arr);
+    }
+    
+    arr.push(root.val);
+    
+    if(root && root.right){
+        BST(root.right, arr);
+    }
+}

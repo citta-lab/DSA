141764c81b23e9c17b780fb34480c3192e2076e6
citta-lab <mahesh2684@gmail.com>
2022-03-09 08:57:44 -0500

    Update copy-random-linkedList.js

--- linkedList/copy-random-linkedList.js
+++ linkedList/copy-random-linkedList.js
@@ -1,4 +1,7 @@
 /**  
+ *
+ * 138. Copy List with Random Pointer
+ *
  * A linked list of length n is given such that each node contains an additional random pointer, 
  * which could point to any node in the list, or null. Construct a deep copy of the list. 
  * The deep copy should consist of exactly n brand new nodes, where each new node has its value set
@@ -7,6 +10,7 @@
  * list represent the same list state. None of the pointers in the new list should point to nodes 
  * in the original list.
  * 
+ * leetcode-question:138
  * leetcode: https://leetcode.com/problems/copy-list-with-random-pointer/
  * video: https://www.youtube.com/watch?v=5Y2EiZST97Y
  * 

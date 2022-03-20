27dc711db1da90d0c0b8c4f0745d356f7c4d7391
mahesh <mahesh2684@gmail.com>
2022-03-20 17:34:41 -0400

    BST: March 20th 2022

---
+++ arrays/1570.dot-product-of-two-sparse-vectors.js
@@ -0,0 +1,71 @@
+/** 
+ * 1570. Dot Product of Two Sparse Vectors  
+ * 
+ * Given two sparse vectors, compute their dot product.
+ * 
+ * Implement class SparseVector:
+ * SparseVector(nums) Initializes the object with the vector nums
+ * dotProduct(vec) Compute the dot product between the instance of SparseVector and vec
+ * A sparse vector is a vector that has mostly zero values, you should store the sparse 
+ * vector efficiently and compute the dot product between two SparseVector.
+ * 
+ * Follow up: What if only one of the vectors is sparse?
+ * 
+ * Input: nums1 = [1,0,0,2,3], nums2 = [0,3,0,4,0]
+ * Output: 8
+ * 
+ * Input: nums1 = [0,1,0,0,0], nums2 = [0,0,0,0,2]
+ * Output: 0
+ * 
+ * leetcode-question:1570
+ * leetcode:https://leetcode.com/problems/dot-product-of-two-sparse-vectors/
+ * company: FACEBOOK
+ * 
+ * Hint:
+ * - Time:O(min(M,N)) and Space:O(min(M,N))
+ * - Use hash in contructor to have "index":"value" pair for all values other than ZERO
+ * -- doing so will help us not worry about 0 as multiplying with 0 will give 0
+ * - In product method we access this.hash and loop though all the keys which 
+ * matches with "array2.hash[key]" and then calculate the SUM
+ */
+
+/** Time: O(min(M,N)) and Space: O(min(M,N)) for hash */
+var SparseVector = function(nums) {
+    this.nums = nums;
+    this.hash = {};
+    
+    /** have every values greater than 0 in hash as key:value */
+    for(let i=0; i<this.nums.length; i++){
+        let num = this.nums[i];
+        if(num !== 0){
+            this.hash[i] = num;
+        }
+    }
+    
+    console.log(this.hash);
+};
+
+// Return the dotProduct of two sparse vectors
+/**
+ * @param {SparseVector} vec
+ * @return {number}
+ */
+SparseVector.prototype.dotProduct = function(vec) {
+    let sum = 0;
+    
+    /** access hash of first array v1 */
+    for(let key in this.hash){
+        let value = this.hash[key];
+        /** access the hash of second array v2 */
+        if(vec.hash[key]){
+            sum = sum + (value * vec.hash[key]);
+        }
+    }
+    
+    return sum
+};
+
+// Your SparseVector object will be instantiated and called as such:
+// let v1 = new SparseVector(nums1);
+// let v2 = new SparseVector(nums2);
+// let ans = v1.dotProduct(v2);
---
+++ binary-search-tree/938.range-sum-of-bst.js
@@ -0,0 +1,44 @@
+/** 
+ * 938. Range Sum of BST 
+ * 
+ * Given the root node of a binary search tree and two integers low and high, 
+ * return the sum of values of all nodes with a value in the inclusive range [low, high]. 
+ * 
+ * Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+ * Output: 32
+ * 
+ * Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+ * Output: 23
+ * 
+ * leetcode-question:938
+ * leetcode:https://leetcode.com/problems/range-sum-of-bst/
+ * company:FACEBOOK
+ * 
+ * Hint:
+ * - Time:O(n) and Space:O(n)
+ * - use pre-order recursion and before calling left and right 
+ * check if the low < root.val and high > root.val respectively.
+ * 
+ * 
+ * */
+
+ /** Time:O(N) and Space:O(N) */
+ var rangeSumBST = function(root, low, high) {
+    let sum =0;
+    preorder(root, low, high);
+    return sum;
+    
+    function preorder(root, low, high){
+        let stack = [root];
+        while(stack.length){
+            let node = stack.pop();
+            
+            if(node.val >= low && node.val <= high){
+                sum += node.val;
+            }
+            
+            if(node && node.left && low < node.val) stack.push(node.left);
+            if(node && node.right && high > node.val) stack.push(node.right);
+        }
+    }
+};

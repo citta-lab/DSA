c462a49ba25aab41df819eb69254dd99cc733f32
mahesh <mahesh2684@gmail.com>
2022-03-29 23:28:21 -0400

    subarray

--- arrays/560.subarray-sum-equals-K.js
+++ arrays/560.subarray-sum-equals-K.js
@@ -40,3 +40,41 @@ var subarraySum = function(nums, k) {
     return count
 };
 
+/** Time:O(N) and Space:O(N) */
+/**
+ * Hint:
+ * - Prefix Lookup: we will take each number and subtract by k (i.e num[i] - k )
+ * and check if we have this "prefix" in Map. If yes then increase the COUNT.
+ * -- i.e preFix = num[i] - k;
+ * - Sum: this will be cumulative SUM += num[i] which will find and add it to 
+ * same Map. 
+ * - Important: Prefix is used to do Map lookup and increase the count. Sum is
+ * calculated and added to the Map.
+ */
+
+ var subarraySum = function(nums, k) {
+    let count = 0;
+    let sum =0;
+    
+    /** map to keep hold of prefixSum : count */
+    let map = new Map();
+    /** base condition to start with so when we have prefixSum - k = 0 we
+    would want to count. example: 3-3 = 0 then we can map.get(0) + 1 */
+    map.set(0, 1);  /** it will be {0:1, 1:1, 2:1} etc */  
+    
+    for(let i=0; i<nums.length; i++){
+        
+        let num = nums[i];
+        sum += num;
+        
+        if(map.has(sum-k)){
+            count += map.get(sum-k);
+        }
+        
+        if(!map.has(sum)) map.set(sum, 0);
+        map.set(sum, map.get(sum) + 1);
+    }
+    
+    return count
+};
+

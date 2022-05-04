66add3c69317888e66da35f93c1729dfc123127f
citta-lab <mahesh2684@gmail.com>
2022-05-04 06:47:08 -0400

    Update 042.trapping-rain-water.js

--- dp/042.trapping-rain-water.js
+++ dp/042.trapping-rain-water.js
@@ -6,22 +6,32 @@
  * compute how much water it can trap after raining.
  * 
  * Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
-    Output: 6
-    Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. 
-    In this case, 6 units of rain water (blue section) are being trapped.
- */
-
-/**
+ *  Output: 6
+ *   Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. 
+ *   In this case, 6 units of rain water (blue section) are being trapped.
+ *
+ * BruteForce:: Time(O^2) and Space:O(N)
+ * - To find left and right Max we will need to itertative over all left and right elements everytime
+ * with respect to the index we are finding the water. Which is O(N^2)
+ *
+ * Optimal (Dynamic Programming):: Time:O(N) and Space:O(N)
+ * - Instead of calculating all leftMax and rightMax every time, we will hold the max value in temp array
+ * - By doing so we will use extra space
+ *
+ * Code Flow: 
  * There are three important steps we need to take.
  * - need to find out all the left max height from every index ( go from 0 to length)
  * - need to find right max height for every index ( go from length to 0)
  * - loop throuhg each index to find how much water we can trap. 
  * i.e min of left and right building height then the acualt building height
  * leet code:
- * https://leetcode.com/problems/trapping-rain-water/solution/
+ * 
+ * 
+ * leetcode-question: 042
+ * leetcode:https://leetcode.com/problems/trapping-rain-water/solution/
  */
 
- // O(n) space and time
+/** Dynamic Programming: Time:O(N) and Space:O(N) */
 var trap = function(height) {
     // find left max
     // right max 
@@ -39,6 +49,7 @@ var trap = function(height) {
  
 };
 
+/** temp will hold the max left upto that point */
 const findLeftMax = (array, temp=[]) => {
    temp[0] = array[0];
    for(let i=1; i<array.length; i++){
@@ -49,6 +60,7 @@ const findLeftMax = (array, temp=[]) => {
     return temp;
 }
 
+/** temp will hold the max right upto that point */
 const findRightMax = (array, temp=[]) => {
     temp[array.length-1] = array[array.length-1];
     for(let i=array.length-2; i>=0; i--){

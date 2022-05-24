5c14d9908a5ebe85503c3d03bc65a6e351ed715b
citta-lab <mahesh2684@gmail.com>
2022-05-23 20:11:09 -0400

    Update 540.single-element-in-a-sorted-array.js

--- sort/540.single-element-in-a-sorted-array.js
+++ sort/540.single-element-in-a-sorted-array.js
@@ -1,19 +1,83 @@
 /**
  * 540. Single Element in a Sorted Array
- *
- * You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once.
+ * 
+ * You are given a sorted array consisting of only integers where every element 
+ * appears exactly twice, except for one element which appears exactly once.
  * Return the single element that appears only once.
  * Your solution must run in O(log n) time and O(1) space.
  * 
  * Input: nums = [1,1,2,3,3,4,4,8,8]
  * Output: 2
- *
+ * 
+ * Input: nums = [3,3,7,7,10,11,11]
+ * Output: 10
+ * 
  * leetcode:https://leetcode.com/problems/single-element-in-a-sorted-array/
- * leetcode-question:540 
- *
+ * leetcode-question:540
+ * 
  * Hint:
+ * BruteForceI: Time:O(N) and Space:(N)
+ * - map to hold the key and it's count 
+ * - map.entries() will return pair so we can check if value === 1 return key
+ * 
+ * BruteForceII ( Two Pointer) : Time:(N) and Space:O(1)
+ * - will have left = 0 and right = left + 1
+ * - while right < nums.length-1 ( so we dont cross )
+ * - if(nums[left] !== nums[right]) we return nums[left];
+ * 
+ * Binary Searcg:: Time:O(logN) and Space:O(1)
  */
 
+/*******************************************************************
+ * 
+ * BruteForce : Time: O(N) and Space:O(N
+ * 
+ *******************************************************************/
+/** Time: O(N) and Space:O(N) */
+var singleNonDuplicate = function(nums) {
+    let map = new Map();
+    for(let num of nums){
+        if(!map.has(num)) map.set(num, 0);
+        map.set(num, map.get(num)+1);
+    }
+    
+    for(let pair of map.entries()){
+        const [key, value] = pair;
+        if(value === 1) return key;
+    }
+};
+
+
+/*******************************************************************
+ * 
+ * BruteForce (Two Pointer): Time: O(N) and Space:O(1)
+ * 
+ *******************************************************************/
+var singleNonDuplicate = function(nums) {
+    
+    if(nums.length == 1) return nums[0]
+    
+    let left = 0;
+    let right = left+1;
+    
+    while(right < nums.length - 1){
+        if(nums[left] !== nums[right]){
+            return nums[left];
+        }
+        
+        left = right + 1;
+        right = left + 1;
+    }
+    
+    return nums[nums.length-1];
+};
+
+
+/*******************************************************************
+ * 
+ * Binary Search : Time: O(logN) and Space:O(1)
+ * 
+ *******************************************************************/
 var singleNonDuplicate = function(nums) {
     
     if(nums.length === 1) return nums[0];

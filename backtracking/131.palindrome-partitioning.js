eed3b818acccdf271825e769cd3266c46383bddb
mahesh <mahesh2684@gmail.com>
2022-06-01 18:05:09 -0400

    palindrome

---
+++ backtracking/131.palindrome-partitioning.js
@@ -0,0 +1,70 @@
+/**
+ * 131. Palindrome Partitioning
+ * 
+ * Given a string s, partition s such that every substring of 
+ * the partition is a palindrome. Return all possible palindrome 
+ * partitioning of s.
+ * 
+ * A palindrome string is a string that reads 
+ * the same backward as forward.
+ * 
+ * Example:
+ * Input: s = "aab"
+ * Output: [["a","a","b"],["aa","b"]]
+ * 
+ * leetcode:https://leetcode.com/problems/palindrome-partitioning/
+ * leetcode-question:131
+ * 
+ * Hint:
+ * Backtrack - Time:O(N * 2^N) and Space:O(N)
+ * - dfs 
+ * - need to send arr, subarray, and start = 0
+ * - if start >= s.length we add copy of subarray to arr
+ * - for loop starts with i=start, which we will use as end in dfs (i.e i)
+ * - check if we find palindrome, if yes call in recursion
+ */
+
+ var partition = function(s) {
+    let result = [];
+     
+    let index = 0;
+    let subArray = [];
+    dfs(s, result, subArray, index);
+    return result;
+ };
+ 
+ 
+ function dfs(s, result, subArray, start){
+     
+     if(start >= s.length) {
+         let copy = [...subArray];
+         result.push(copy);
+     }
+     
+     for(let i=start; i<s.length; i++){
+         let checkPalindrome = isPalindrome(s, start, i);
+         
+         if(checkPalindrome){
+             
+             subArray.push(s.substring(start, i+1));
+             dfs(s, result, subArray, i+1); // <-- important to do i+1 rather than start+1 
+             
+             /**backtrack*/
+             subArray.pop();
+         }
+     }
+ }
+ 
+ 
+ function isPalindrome(s, left, right){
+     while(left < right){
+         if(s[left] !== s[right]){
+             return false
+         }
+         
+         left++
+         right--
+     }
+     
+     return true
+ }
--- byType/palindrome.md
+++ byType/palindrome.md
@@ -12,13 +12,27 @@ Hint: start left,right at 0 but move then outward. use for loop and then two
 whileloops one for even, one for odd
 
 ### 3. 9. Palindrome Number   
-Question: https://leetcode.com/problems/palindrome-number/   
-Answer: 
+Question: https://leetcode.com/problems/palindrome-number/       
+Answer: https://github.com/citta-lab/DSA/blob/main/math/009.palindrome-number.js
 
 ### 4. 234. Palindrome Linked List
 Question: https://leetcode.com/problems/palindrome-linked-list/submissions/   
-Answer:    
+Answer: https://github.com/citta-lab/DSA/blob/main/linkedList/234.palindrome-linked-list.js      
 Hint: if we split linkedList by half, reverse secondHalf, then compare will
 yiled in O(1) space
+
+### 5. 647. Palindromic Substrings
+Question: https://leetcode.com/problems/palindromic-substrings/   
+Answer: https://github.com/citta-lab/DSA/blob/main/strings/647.palindromic-substrings.js   
+Hint: can be done using forloop and two whileloop or backtracking (similar to longest palindrome substring)
+
+### 6. 131. Palindrome Partitioning
+Question: https://leetcode.com/problems/palindrome-partitioning/    
+Answer: 
+Hint: backtracking with forloop 
+
+
+
+
 
 
